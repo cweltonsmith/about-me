d55394b33c2fb8668d5fe9d026774891946a6b6b
cweltonsmith <c.welton.smith@gmail.com>
2019-09-19 19:04:30 -0500

    Completed App

---
+++ SCRIPT/code.js
@@ -0,0 +1,16 @@
+function calculations() {
+    
+    //Passes string and returns float
+    const weight = parseFloat(document.querySelector('#weight-field').value)
+    const moment = parseFloat(document.querySelector('#moment-field').value)
+
+    //Doing all the simple calculations
+    const CG = (moment / weight)
+
+    //Modifying HTML page with the calculations output
+    document.querySelector('#total-amount').innerHTML = ` ${CG.toFixed(2)}`
+}
+
+
+//On click perform the calculation function
+document.querySelector('#calculate').addEventListener('click', calculations)
